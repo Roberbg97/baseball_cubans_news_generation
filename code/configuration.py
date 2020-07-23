04c75ad378a4e113f0f2f68dd8c5830c2fc3b52d
Roberto Balboa Gonzalez <r.balboa@estudiantes.matcom.uh.cu>
2020-07-23 17:52:45 -0400

    Añadido el nuevo clasificador

--- code/configuration.py
+++ code/configuration.py
@@ -1,5 +1,12 @@
+import os
+try:
+    MODULE = os.path.dirname(os.path.realpath(__file__))
+except:
+    MODULE = ""
+
 from first_algorithms_flow import Scrapper_BR, Outstandings_LR, New_Templates
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+
 
 def config():
     s = Scrapper_BR()
@@ -15,7 +22,7 @@ def config():
     return nt.get_text()
 
 env = Environment(
-    loader = FileSystemLoader('./templates'),
+    loader = FileSystemLoader(os.path.join(MODULE,'templates')),
     autoescape = select_autoescape(['html', 'xml'])
 )
 
@@ -25,6 +32,6 @@ title, text = config()
 
 template = template.render(title=title, text=text)
 
-with open('../index.html', 'w') as h:
+with open(os.path.join(MODULE,'index.html'), 'w') as h:
     h.write(template)
     h.close()
