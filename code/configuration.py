446e2473dae2f23039e3518ba3c8fdcdc6ecee82
Roberto Balboa Gonzalez <r.balboa@estudiantes.matcom.uh.cu>
2020-07-30 16:20:18 -0400

    Añadido archivo de configuracion

--- code/configuration.py
+++ code/configuration.py
@@ -1,35 +1,67 @@
+import json
+from importlib import import_module
+from configparser import ConfigParser
+#from first_algorithms_flow import Outstandings_LR, New_Templates
+#from jinja2 import Environment, FileSystemLoader, select_autoescape
+#from notice_pipeline.scrapper import Scrapper_BR
+#from past_news import Renderer
 import os
 try:
     MODULE = os.path.dirname(os.path.realpath(__file__))
 except:
     MODULE = ""
 
-import json
-from first_algorithms_flow import Outstandings_LR, New_Templates
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-from notice_pipeline.scrapper import Scrapper_BR
-from past_news import Renderer
 
 def config():
 
     cubans = json.load(open(os.path.join(MODULE, 'cubans.json'), 'r'))
 
-    #s = Scrapper_BR()
-    s = Scrapper_BR(cubans)
+    parser = ConfigParser()
+    parser.read(os.path.join(MODULE, '..', 'config.ini'))
+
+    scraping_module = parser.get('scraping', 'module')
+    scraping_class = parser.get('scraping', 'class')
+
+    clasification_module = parser.get('clasification', 'module')
+    clasification_class = parser.get('clasification', 'class')
+
+    gen_module = parser.get('generation', 'module')
+    gen_class = parser.get('generation', 'class')
+
+    render_module = parser.get('render', 'module')
+    render_class = parser.get('render', 'class')
+
+    module = import_module(scraping_module)
+    class_ = getattr(module, scraping_class)
+    s = class_(cubans)
+
+    #s = Scrapper_BR(cubans)
 
     #player_details = s.scraping()
     d = s._scrap()
 
-    o = Outstandings_LR()
+
+    module = import_module(clasification_module)
+    class_ = getattr(module, clasification_class)
+    o = class_()
+    #o = Outstandings_LR()
 
     sfo = o.get_sorted_outstandings(d['game_day_data'])
 
+
+    module = import_module(gen_module)
+    class_ = getattr(module, gen_class)
+    nt = class_(d['game_day_data'], sfo, d['all_games_details'])
+
     #nt = New_Templates(player_details, sfo)
-    nt = New_Templates(d['game_day_data'], sfo, d['all_games_details'])
+    #nt = New_Templates(d['game_day_data'], sfo, d['all_games_details'])
 
     title, paragraphs = nt.get_text()
 
-    r = Renderer(title, paragraphs)
+    module = import_module(render_module)
+    class_ = getattr(module, render_class)
+    r = class_(title, paragraphs)
+    #r = Renderer(title, paragraphs)
 
     r.render()
 
@@ -52,4 +84,4 @@ with open(os.path.join(MODULE,'..','index.html'), 'w') as h:
     h.close()
 '''
 
-title, paragraphs = config()
+config()
